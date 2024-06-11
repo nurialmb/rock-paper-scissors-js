30d31097c38f9f2901ad969c9252050256d8cb6a
nurialmb <128636606+nurialmb@users.noreply.github.com>
2024-06-12 00:29:33 +0500

    Add getComputerChoice function

---
+++ javascript.js
@@ -0,0 +1,16 @@
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
+    if (computerChoice == 0) {
+        computerChoice = "rock"
+    }
+    else if (computerChoice == 1) {
+        computerChoice = "paper"
+    }
+    else {
+        computerChoice = "scissors"
+    }
+    return computerChoice;
+}
+console.log(getComputerChoice());
+
+
